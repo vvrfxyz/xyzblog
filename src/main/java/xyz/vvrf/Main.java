0272d02375223bc86845b3375891350abc8b53f3
温瑞峰 <wenruifeng@sinosoft.com.cn>
2023-08-22 17:56:42 +0800

    Initial commit

---
+++ src/main/java/xyz/vvrf/Main.java
@@ -0,0 +1,13 @@
+package xyz.vvrf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Main.class);
+        app.run(args);
+    }
+}
