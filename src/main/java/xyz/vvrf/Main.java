31f92c91e40f8c2cbec943114a2d2d9fbbe8d603
vvrf <vvrfxyz@gmail.com>
2024-02-25 22:52:33 +0800

    1

--- src/main/java/xyz/vvrf/Main.java
+++ src/main/java/xyz/vvrf/Main.java
@@ -1,7 +1,12 @@
 package xyz.vvrf;
 
+import org.apache.ibatis.session.SqlSessionManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 @SpringBootApplication
 
@@ -9,5 +14,6 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Main.class);
         app.run(args);
+        BlockingQueue blockingQueue = new LinkedBlockingQueue();
     }
 }
