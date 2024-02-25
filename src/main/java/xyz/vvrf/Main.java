c7cdfea82e645b14a450facd8f1f43a384596972
vvrf <vvrfxyz@gmail.com>
2024-02-25 23:29:18 +0800

    4

--- src/main/java/xyz/vvrf/Main.java
+++ src/main/java/xyz/vvrf/Main.java
@@ -10,6 +10,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 @SpringBootApplication
 //3
+//4
 public class Main {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Main.class);
